275d0297cedf7be4074d438778fda73b8fefbd2f
extrawurst <mail@rusticorn.com>
2024-07-08 09:47:26 +0200

    upgrades

--- src/apple.rs
+++ src/apple.rs
@@ -1,8 +1,8 @@
 /// returns first language from `[NSLocale preferredLanguages]`
 /// (see [docs](https://developer.apple.com/documentation/foundation/nslocale/1415614-preferredlanguages))
 pub fn lang_apple() -> Option<String> {
-    use icrate::Foundation::NSString;
     use objc2::{class, msg_send, runtime::NSObject};
+    use objc2_foundation::NSString;
 
     let nslocale = class!(NSLocale);
     let id = unsafe {
