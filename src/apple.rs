6a5b1553550ca4477e9a50e4bf0aeae64a38a720
extrawurst <mail@rusticorn.com>
2025-03-29 15:41:42 +0800

    support iOS < 17

--- src/apple.rs
+++ src/apple.rs
@@ -1,15 +1,9 @@
 /// returns first language from `[NSLocale preferredLanguages]`
 /// (see [docs](https://developer.apple.com/documentation/foundation/nslocale/1415614-preferredlanguages))
 pub fn lang_apple() -> Option<String> {
-    use objc2::{class, msg_send, runtime::NSObject};
-    use objc2_foundation::NSString;
+    use objc2_foundation::NSLocale;
 
-    let nslocale = class!(NSLocale);
-    let id = unsafe {
-        let array: *const NSObject = msg_send![nslocale, preferredLanguages];
-        let id: *const NSString = msg_send![array, objectAtIndex:0_isize];
-        id.as_ref()?
-    };
+    let lang = unsafe { NSLocale::preferredLanguages().iter().next()?.to_string() };
 
-    Some(id.to_string())
+    Some(lang.to_string())
 }
